24b4d0b49296d5287f2c4e8b7125100077105ea4
Joao-Filh0 <joaoadmob2@gmail.com>
2023-03-31 20:08:56 -0300

    squash-1.0.5

--- fsv.rb
+++ fsv.rb
@@ -1,9 +1,9 @@
 class Fsv < Formula
   include Language::Python::Virtualenv
 
-  url "https://github.com/Joao-Filh0/fsv/archive/refs/tags/0.0.4.tar.gz"
-  sha256 "205a06d3836f5cf69b16355011a9174d7b9d14f9c508ece2e82837b6ba5258a5"
-  version "1.0.4"
+  url "https://github.com/Joao-Filh0/fsv/archive/refs/tags/0.0.5.tar.gz"
+  sha256 "fd8ce859f78eaf22ae1c63e4503c526ac729fbcd08b318d2d5254e26110080e5"
+  version "1.0.5"
   head "https://github.com/Joao-Filh0/fsv.git", branch: "main"
 
   depends_on "python@3.10"
