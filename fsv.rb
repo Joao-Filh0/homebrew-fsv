b249dc6324ebc34daa58103566298286fe5951e1
Joao-Filh0 <joaoadmob2@gmail.com>
2023-03-31 19:50:14 -0300

    squash! 1.0.4-fix-permission-15

--- fsv.rb
+++ fsv.rb
@@ -8,21 +8,20 @@ class Fsv < Formula
 
   depends_on "python@3.10"
 
- def install
-  venv = virtualenv_create(libexec, "python3")
-  venv.pip_install_and_link buildpath
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    venv.pip_install_and_link buildpath
 
-  (buildpath/"fsv").write <<~EOS
-    #!/bin/bash
-    exec "#{libexec}/bin/python" "#{libexec}/bin/main.py" "$@"
-  EOS
+    (buildpath/"fsv").write <<~EOS
+      #!/bin/bash
+      exec "#{libexec}/bin/python" "#{libexec}/lib/python3.10/site-packages/fsv/main.py" "$@"
+    EOS
 
-  chmod 0755, buildpath/"fsv"
-  bin.install buildpath/"fsv"
+    chmod 0755, buildpath/"fsv"
+    bin.install buildpath/"fsv"
 
-  libexec.install "fsv/main.py"
-end
-
+    libexec.install "main.py"
+  end
 
   test do
     system "#{bin}/fsv", "-pg"
