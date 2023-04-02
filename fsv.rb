36c82f40bc98d9d0277f4e2c730605690f10c2cf
Joao-Filh0 <joaoadmob2@gmail.com>
2023-04-02 19:11:54 -0300

    color+1

--- fsv.rb
+++ fsv.rb
@@ -1,9 +1,9 @@
 class Fsv < Formula
   include Language::Python::Virtualenv
 
-  url "https://github.com/Joao-Filh0/fsv/archive/refs/tags/0.0.15.tar.gz"
-  sha256 "104153c3bb83f7e9a6feb9f95a055109fcb95a071794a55bebefefbf6acbe9dd"
-  version "1.0.15"
+  url "https://github.com/Joao-Filh0/fsv/archive/refs/tags/0.0.16.tar.gz"
+  sha256 "8f53fa834202e71e7c19836494f053cfa0dc82e88e170c0c9dba1b10c51ba8ef"
+  version "1.0.16"
   head "https://github.com/Joao-Filh0/fsv.git", branch: "main"
 
   depends_on "python@3.10"
@@ -11,7 +11,7 @@ class Fsv < Formula
   def install
     venv = virtualenv_create(libexec, "python3")
     venv.pip_install_and_link buildpath
-    venv.pip_install ["requests","argparse","urllib3","chardet","idna","certifi"]
+    venv.pip_install ["requests","argparse","urllib3","chardet","idna","certifi","colorama"]
 
 
     (buildpath/"fsv").write <<~EOS
